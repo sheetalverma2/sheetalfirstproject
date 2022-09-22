13f8481e46be3aabd673fad1e96ae532eea17ac2
anand <anand@DESKTOP-9JSPD2N.myfiosgateway.com>
2022-09-22 19:39:11 -0400

    Welcome screen

--- src/Training/login.java
+++ src/Training/login.java
@@ -5,6 +5,7 @@ public class login {
 	public static void main(String[] args) {
 		
 		System.out.println("Login to the page to access services");
+		System.out.println("Welcome to CodeLab Home");
 	}
 
 }
